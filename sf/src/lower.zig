ed4b2d70a70b8a409fda18e6f2c9934568540902
Andres <melkyr@github.com>
2026-06-02 19:06:37 -0500

    Fixing subscript errors again

--- sf/src/lower.zig
+++ sf/src/lower.zig
@@ -762,8 +762,8 @@ fn lowerExprImpl(self: *LirLowerer, node_idx: u32) u32 {
                 emitInst(self, LirInst{ .assign = .{ .name_id = @intCast(u32, 0), .dst = reg, .src = src } });
             }
         } else if (child_node.kind == AstKind.index_access) {
-            var base_temp = lowerExpr(self, child_node.child_0);
-            base_temp = maybeExtractSlicePtr(self, child_node.child_0, base_temp);
+            var raw_base = lowerExpr(self, child_node.child_0);
+            var base_temp = maybeExtractSlicePtr(self, child_node.child_0, raw_base);
             var idx_temp = lowerExpr(self, child_node.child_1);
             var bai_m: []const u8 = "BAI:b"; pal.stderr_write(bai_m);
             var bai_bb: [10]u8 = undefined; var bai_bl = itoa_mod.itoa(base_temp, bai_bb[0..]); var bai_bs: usize = @intCast(usize, 9) - @intCast(usize, bai_bl); pal.stderr_write(bai_bb[bai_bs..@intCast(usize, 9)]);
@@ -773,7 +773,9 @@ fn lowerExprImpl(self: *LirLowerer, node_idx: u32) u32 {
             var bai_sb: [10]u8 = undefined; var bai_sl = itoa_mod.itoa(src, bai_sb[0..]); var bai_ss: usize = @intCast(usize, 9) - @intCast(usize, bai_sl); pal.stderr_write(bai_sb[bai_ss..@intCast(usize, 9)]);
             var bai_nl: []const u8 = "\n"; pal.stderr_write(bai_nl);
             var ai_ni: u32 = @intCast(u32, 0);
+            if (base_temp == raw_base) {
             if (store.nodes.items[@intCast(usize, child_node.child_0)].kind == AstKind.ident_expr) { ai_ni = store.identifiers.items[@intCast(usize, store.nodes.items[@intCast(usize, child_node.child_0)].payload)]; }
+            }
             emitInst(self, LirInst{ .assign_index = .{ .name_id = ai_ni, .base = base_temp, .index = idx_temp, .src = src } });
         } else {
             var dst = lowerExpr(self, node.child_0);
@@ -809,12 +811,12 @@ fn lowerExprImpl(self: *LirLowerer, node_idx: u32) u32 {
         emitInst(self, LirInst{ .addr_of = .{ .operand = operand_temp, .result = tid } });
         return tid;
     } else if (node.kind == AstKind.index_access) {
-        var base_temp = lowerExpr(self, node.child_0);
+        var raw_base = lowerExpr(self, node.child_0);
         var msp_m: []const u8 = "MSP:b"; pal.stderr_write(msp_m);
-        var msp_bb: [10]u8 = undefined; var msp_bl = itoa_mod.itoa(base_temp, msp_bb[0..]); var msp_bs: usize = @intCast(usize, 9) - @intCast(usize, msp_bl); pal.stderr_write(msp_bb[msp_bs..@intCast(usize, 9)]);
+        var msp_bb: [10]u8 = undefined; var msp_bl = itoa_mod.itoa(raw_base, msp_bb[0..]); var msp_bs: usize = @intCast(usize, 9) - @intCast(usize, msp_bl); pal.stderr_write(msp_bb[msp_bs..@intCast(usize, 9)]);
         var msp_nm: []const u8 = "n"; pal.stderr_write(msp_nm);
         var msp_nb: [10]u8 = undefined; var msp_nl = itoa_mod.itoa(node.child_0, msp_nb[0..]); var msp_ns: usize = @intCast(usize, 9) - @intCast(usize, msp_nl); pal.stderr_write(msp_nb[msp_ns..@intCast(usize, 9)]);
-        base_temp = maybeExtractSlicePtr(self, node.child_0, base_temp);
+        var base_temp = maybeExtractSlicePtr(self, node.child_0, raw_base);
         var msp2_m: []const u8 = "p"; pal.stderr_write(msp2_m);
         var msp2b: [10]u8 = undefined; var msp2l = itoa_mod.itoa(base_temp, msp2b[0..]); var msp2s: usize = @intCast(usize, 9) - @intCast(usize, msp2l); pal.stderr_write(msp2b[msp2s..@intCast(usize, 9)]);
         var msp_nl2: []const u8 = "\n"; pal.stderr_write(msp_nl2);
@@ -837,7 +839,9 @@ fn lowerExprImpl(self: *LirLowerer, node_idx: u32) u32 {
         }
         var tid = nextTemp(self, elem_type[0]);
         var li_ni: u32 = @intCast(u32, 0);
+        if (base_temp == raw_base) {
         if (store.nodes.items[@intCast(usize, node.child_0)].kind == AstKind.ident_expr) { li_ni = store.identifiers.items[@intCast(usize, store.nodes.items[@intCast(usize, node.child_0)].payload)]; }
+        }
         emitInst(self, LirInst{ .load_index = .{ .name_id = li_ni, .base = base_temp, .index = idx_temp, .result = tid } });
         return tid;
     } else if (node.kind == AstKind.ident_expr) {
