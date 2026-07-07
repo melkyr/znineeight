5086ee2af33dd976a102549ffc669b407b1915c2
zni-0004 <zni@z98>
2026-07-07 17:29:15 +0000

    test(repro): struct-init slice-field field-value-load RED repro

---
+++ repro/struct_init_slice_field/main.zig
@@ -0,0 +1,14 @@
+extern fn __bootstrap_print_int(i: i32) void;
+
+const Tokenizer = struct { input: []const u8, pos: usize };
+
+pub fn main() void {
+    var src: [8]u8 = undefined;
+    var i: usize = 0;
+    while (i < 8) { src[i] = @intCast(u8, i + 97); i += 1; }
+    const line = src[0..4];
+    var t = Tokenizer{ .input = line, .pos = 0 };
+    __bootstrap_print_int(@intCast(i32, t.input.len));
+    __bootstrap_print_int(@intCast(i32, t.input[0]));
+    __bootstrap_print_int(@intCast(i32, t.pos));
+}
