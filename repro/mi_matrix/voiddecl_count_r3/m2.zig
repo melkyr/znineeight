f35e18350e62f93295df036bdeb8a1d6b39131b4
zni-0004 <zni@z98>
2026-08-18 22:30:25 +0000

    repro: sibling module count probe (voiddecl_count_r3)

---
+++ repro/mi_matrix/voiddecl_count_r3/m2.zig
@@ -0,0 +1,2 @@
+pub const S = struct { v: u32 };
+pub fn make() S { var f = S{ .v = 2 }; return f; }
---
+++ repro/mi_matrix/voiddecl_count_r3/m22.zig
@@ -0,0 +1,2 @@
+pub const S = struct { v: u32 };
+pub fn make() S { var f = S{ .v = 22 }; return f; }
