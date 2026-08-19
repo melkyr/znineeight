069b6b355d6d65ccd1b29b05bc8f274b3e409d02
zni-0004 <zni@z98>
2026-08-19 11:55:18 +0000

    repro: extra_children 65536 boundary probe (voiddecl_boundary_xmod)

---
+++ repro/mi_matrix/voiddecl_boundary_xmod/main.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const m1 = @import("m1.zig");
+const m2 = @import("m2.zig");
+const m3 = @import("m3.zig");
+const m4 = @import("m4.zig");
+const m5 = @import("m5.zig");
+const m6 = @import("m6.zig");
+const m7 = @import("m7.zig");
+pub fn main() void {
+    var a = m1.make();
+    var b = m7.make();
+    std.io.printInt(a.v);
+    std.io.writeByte(@intCast(u8, ' '));
+    std.io.printInt(b.v);
+}
